a2b839cc69da6ded5cf212f94a27484985dc5be3
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2019-03-06 22:43:03 +0100

    Initial commit

---
+++ src/test/java/FirstFireFoxTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FirstFireFoxTest {
+
+    @Test
+    public void firefoxIsSupportedByWebdriver() {
+
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://www.compendiumdev.co.uk/selenium");
+
+        assertTrue(driver.getTitle().startsWith("Selenium Simplified"));
+        driver.close();
+    }
+    @Test
+    public void firefoxIsSupportedByWebdriverSetUpSystemProperty() {
+
+        String currentDirectory = System.getProperty("user.dir");
+        System.out.println(currentDirectory);
+
+        // If using driver location next to current directory than use below.
+        // String driverLocation = currentDirectory + "/../tools/drivers/geckodriver.exe"
+        String driverLocation = "C:\\ProgramData\\chocolatey\\bin\\geckodriver.exe";
+        System.setProperty("webdriver.gecko.driver", driverLocation);
+
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://www.compendiumdev.co.uk/selenium");
+
+        assertTrue(driver.getTitle().startsWith("Selenium Simplified"));
+        driver.close();
+    }
+}
---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,54 @@
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FirstTest {
+
+    @Test
+    public void testAppAssertTrue() {
+        assertTrue(true);
+    }
+    @Test
+    public void driverIsKingWithWebdriver() {
+
+        WebDriver driver = new HtmlUnitDriver();
+        driver.get("http://www.compendiumdev.co.uk/selenium");
+
+        assertTrue(driver.getTitle().startsWith("Selenium Simplified"));
+
+    }
+    @Test
+    public void driverIsKingWithHtmlDriver() {
+
+        HtmlUnitDriver driver = new HtmlUnitDriver();
+        driver.get("http://www.compendiumdev.co.uk/selenium");
+
+        assertTrue(driver.getTitle().startsWith("Selenium Simplified"));
+
+    }
+    @Test
+    public void driverIsKingExploreDriverOptions() {
+
+//        HtmlUnitDriver driver = new HtmlUnitDriver();
+
+        WebDriver driver = new HtmlUnitDriver();
+        driver.get("http://www.compendiumdev.co.uk/selenium");
+
+        System.out.println(((HtmlUnitDriver) driver).getBrowserVersion());
+        System.out.println(driver.getCurrentUrl());
+        System.out.println(driver.getPageSource());
+        System.out.println(((HtmlUnitDriver) driver).getCapabilities());
+        System.out.println(((HtmlUnitDriver) driver).getKeyboard());
+        System.out.println(((HtmlUnitDriver) driver).getMouse());
+        System.out.println(driver.getWindowHandle());
+
+
+        assertTrue(driver.getTitle().startsWith("Selenium Simplified"));
+
+    }
+}
